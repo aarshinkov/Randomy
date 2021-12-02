6ebd4bcbf0dcddaf6260f4f76a5d0ab473384b7f
Atanas Arshinkov <aarshinkov9705@gmail.com>
2021-12-03 00:54:24 +0200

    Added Java docs

--- src/main/java/com/aarshinkov/random/Randomy.java
+++ src/main/java/com/aarshinkov/random/Randomy.java
@@ -2,6 +2,14 @@ package com.aarshinkov.random;
 
 import java.security.SecureRandom;
 
+/**
+ * Class for generating random strings with specified length.
+ * <br/><br/>
+ * Also checks if a specified string sequence consist only of lowercase or uppercase letters and if it contains only letters or letters and digits.
+ *
+ * @author Atanas Yordanov Arshinkov
+ * @since 1.0.0
+ */
 public class Randomy {
 
   private final java.util.Random RANDOM = new SecureRandom();
@@ -10,14 +18,27 @@ public class Randomy {
   private final String LOWERCASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
   private final String ALPHABET = CAPITAL_ALPHABET + LOWERCASE_ALPHABET;
   private final String SPECIAL_CHARACTERS = "~`'\"!@#$%^&*()-_+=,.;\\|{}[]";
-//  private final String DIG_ALPHA = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
   private Randomy() {
 
   }
 
+  /**
+   * Used as a generator for new Randomy objects.
+   *
+   * @author Atanas Yordanov Arshinkov
+   * @since 1.0.0
+   */
   public static class Builder {
 
+    /**
+     * Builds a new randomy object
+     *
+     * @return new instance of the Randomy object
+     *
+     * @author Atanas Yordanov Arshinkov
+     * @since 1.0.0
+     */
     public static Randomy build() {
       return new Randomy();
     }
@@ -30,6 +51,9 @@ public class Randomy {
    * @param length the length of the string as characters
    *
    * @return the randomly generated string
+   *
+   * @author Atanas Yordanov Arshinkov
+   * @since 1.0.0
    */
   public String generateRandomString(int length) {
 
@@ -48,6 +72,9 @@ public class Randomy {
    * @param includeDigits if is true it will include digits in the string
    *
    * @return the randomly generated string
+   *
+   * @author Atanas Yordanov Arshinkov
+   * @since 1.0.0
    */
   public String generateRandomString(int length, boolean includeDigits) {
 
@@ -62,10 +89,15 @@ public class Randomy {
    * Takes length as a parameter and returns a random string with the english
    * alphabet
    *
-   * @param length        the length of the string as characters
-   * @param includeDigits if is true it will include digits in the string
+   * @param length           the length of the string as characters
+   * @param includeDigits    if is true it will include digits in the string
+   * @param includeCapital   marks if the generated string should include capital letters
+   * @param includeLowercase marks if the generated string should include lowercase letters
    *
    * @return the randomly generated string
+   *
+   * @author Atanas Yordanov Arshinkov
+   * @since 1.0.0
    */
   public String generateRandomString(int length, boolean includeDigits, boolean includeCapital, boolean includeLowercase) {
     if (length < 0) {
@@ -101,6 +133,13 @@ public class Randomy {
     return String.valueOf(builder);
   }
 
+  /**
+   * Checks if the given string sequence contains at least one digit.
+   *
+   * @param sequence the sequence to be checked
+   *
+   * @return true - if the sequence contains at least one digit; false - if not
+   */
   public boolean hasDigitInString(String sequence) {
 
     if (sequence == null) {
@@ -122,15 +161,41 @@ public class Randomy {
     return false;
   }
 
+  /**
+   * Checks if a given string sequence consists only of lowercase symbols.
+   *
+   * @param sequence the sequence to be checked
+   *
+   * @return true - if the sequence contains only lowercase symbols; false - if it does not
+   *
+   * @since 1.1.0
+   */
   public boolean hasOnlyLowercase(String sequence) {
 
     return hasOnlyOneCase(sequence, false);
   }
 
+  /**
+   * Checks if a given string sequence consists only of uppercase symbols.
+   *
+   * @param sequence the sequence to be checked
+   *
+   * @return true - if the sequence contains only uppercase symbols; false - if it does not
+   *
+   * @since 1.1.0
+   */
   public boolean hasOnlyUppercase(String sequence) {
     return hasOnlyOneCase(sequence, true);
   }
 
+  /**
+   * @param sequence    the sequence to be checked
+   * @param isUppercase marks if the sequence should contain only uppercase (if it is true) or only lowercase (if it is false)
+   *
+   * @return true - if the sequence contains only one type of case (upper or lower)
+   *
+   * @since 1.1.0
+   */
   private boolean hasOnlyOneCase(String sequence, boolean isUppercase) {
     if (sequence == null || sequence.trim().isEmpty()) {
       return false;
